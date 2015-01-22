01c0f6f5aa899e897094f47c9269a10b339bea3b
Dave O'Dwyer <dodwyer91@gmail.com>
2015-01-22 23:08:50 +0000

    Updated start.php file. Added 'server' to local env array

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('DAVE-LINUX', 'DaveAttic-PC', 'dave-linuxpc')
+	'local' => array('DAVE-LINUX', 'DaveAttic-PC', 'dave-linuxpc', 'server')
 
 ));
 
