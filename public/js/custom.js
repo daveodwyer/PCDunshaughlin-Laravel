598c4fe50ca0a9da77a7070ebd430e53edd81544
Dave O'Dwyer <dodwyer91@gmail.com>
2015-01-23 01:08:54 +0000

    Working on validation for Contact form.

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,27 +1,49 @@
 $(document).ready(function() {
- 
-	positionFB();
+
+    if($('#contact-form').length > 0) {
+
+        $('#contact-form').validate({
+
+            rules: {
+                name : 'required',
+                email : {
+                    'required' : true,
+                    'email' : true
+                },
+
+                query : 'required'
+
+            },
+
+            messages: {
+
+                name : 'Please enter your name',
+                email : {
+                    'required' : 'Please enter your email address',
+                    'email' : 'Please enter a valid email address'
+                },
+
+                query : 'Please enter a query'
+
+            },
+
+            errorPlacement: function(error, element) {
+                var element_id = $(element).attr('id');
+                error.prependTo(element.parent('div'));
+            },
+
+            invalidHandler: function(event, validator) {
+
+                var errors = validator.numberOfInvalids();
+                if(errors) {
+                    return false;
+                }
+
+            }
+
+        });
+
+
+    }
 
 });
-
-$(window).resize(function() {
-
-	positionFB();
-	      		
-});
-
-function positionFB() {
-
-	$nav_containter_margin = $('.navbar .container:first').css('margin-right');
-	$nav_containter_width = $('.navbar .container').css('width');
-	console.log($nav_containter_margin);
-	console.log($nav_containter_width);
-
-
-
-	$facebook_right_val = parseInt($nav_containter_margin.replace('px', '')) + parseInt($nav_containter_width.replace('px', ''));
-	console.log($facebook_right_val);
-	$facebook_right_val = $facebook_right_val + 30;
-
-	$('.fb-like-desktop').css('right', $facebook_right_val + "px");
-}
